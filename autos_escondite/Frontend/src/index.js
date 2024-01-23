5e0d6094d24509a7031f1f5210236606a86345cd
Bilal Gillani <bilalgillani1000@gmail.com>
2024-01-23 19:30:32 +0500

    Create signin and signup pages

--- autos_escondite/Frontend/src/index.js
+++ autos_escondite/Frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/home';
+import Signin from './pages/signin';
+import SignUp from './pages/signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Using Concurrent Mode with createRoot
 root.render(
   <React.StrictMode>
-    <Home />
+    {/* <Home /> */}
+    <Signin />
+    {/* <SignUp /> */}
   </React.StrictMode>,
 );
---
+++ autos_escondite/Frontend/src/pages/signup.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import img from "../content/images/download.jpeg";
+
+const SignUp = () => {
+  return (
+    <Container fluid className="p-0" style={{backgroundImage: `url(${img})`, backgroundSize: 'cover', minHeight: '100vh'}}>
+      <Row className="justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <Col md={6}>
+          <Form>
+            <h3 className="text-center mb-4">Sign Up</h3>
+            <Form.Group>
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your full name" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter your password" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Confirm your password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" block>
+              Sign Up
+            </Button>
+          </Form>
+          <a href='/'>Already have an account?Sign In</a>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default SignUp;
